3ce3d51639aa5fc83274c739628e16f7e35c9b9f
smith-sterling <104526450+Viper-Fist@users.noreply.github.com>
2023-01-30 07:24:53 -0700

    Pushing to github

--- src/pack/Main.java
+++ src/pack/Main.java
@@ -1,10 +1,27 @@
+package pack;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
+    public static File file = new File("./src/pack/adjectives.txt");
+
     public static void main(String[] args) {
-        File file = new File();
+        try {
+            ArrayList<String> strings = new ArrayList<>();
+            if (file.exists()) {
+                Scanner scanner = new Scanner(file);
+                while (scanner.hasNextLine()) {
+                    strings.add(scanner.nextLine());
+                }
+                System.out.println(strings.get(new Random().nextInt(strings.size())));
+            }
+        } catch(FileNotFoundException e) {
+            System.out.println("Error");
+        }
     }
-
-
 }
