756b1cdb380de2d6d7d0a65fe40976d1d2f1d2b9
smith-sterling <104526450+Viper-Fist@users.noreply.github.com>
2023-04-27 09:41:23 -0600

    Cleaned up the code a bit and got that no-repeat days stuff working

--- src/Main.java
+++ src/Main.java
@@ -7,85 +7,26 @@ import java.util.*;
 
 public class Main {
 
-    public static File file = new File("/Users/sterlite/Desktop/PetProjects/randomAdjectiveGenerator/src/adjectives.txt");
-    public static File file2 = new File("/Users/sterlite/Desktop/PetProjects/randomAdjectiveGenerator/src/otherList.txt");
-    public static File file3 = new File("/Users/sterlite/Desktop/PetProjects/randomAdjectiveGenerator/src/personAdjectives.txt");
-    public static File file4 = new File("/Users/sterlite/Desktop/PetProjects/randomAdjectiveGenerator/src/untrimmedMasterList.txt");
-    public static File file5 = new File("/Users/sterlite/Desktop/PetProjects/randomAdjectiveGenerator/src/masterList.txt");
+    private static final File file = new File("/Users/sterlite/Desktop/PetProjects/randomAdjectiveGenerator/src/adjectives.txt");
+    private static final File file2 = new File("/Users/sterlite/Desktop/PetProjects/randomAdjectiveGenerator/src/otherList.txt");
+    private static final File file3 = new File("/Users/sterlite/Desktop/PetProjects/randomAdjectiveGenerator/src/personAdjectives.txt");
+    private static final File file4 = new File("/Users/sterlite/Desktop/PetProjects/randomAdjectiveGenerator/src/untrimmedMasterList.txt");
+    private static final File file5 = new File("/Users/sterlite/Desktop/PetProjects/randomAdjectiveGenerator/src/masterList.txt");
 
     public static void main(String[] args) {
-        if (!file.exists()) System.out.println("No file1");
-        if (!file2.exists()) System.out.println("No file2");
-        if (!file3.exists()) System.out.println("No file3");
-        if (!file4.exists()) System.out.println("No file4");
-        if (!file5.exists()) System.out.println("No file5");
-        /*
-        Set<String> strings = new TreeSet<>();
-        Scanner scanner = null;
-        try {scanner = new Scanner(file);} catch(FileNotFoundException ignore) {}
-        while (scanner.hasNextLine()) strings.add(capFirstLetter(scanner.nextLine()));
-        System.out.println("After adjectives: " + strings.size());
-
-        try {scanner = new Scanner(file2);} catch(FileNotFoundException ignore) {}
-        while (scanner.hasNextLine()) strings.add(capFirstLetter(scanner.nextLine()));
-        System.out.println("After otherList: " + strings.size());
-
-        try {scanner = new Scanner(file3);} catch(FileNotFoundException ignore) {}
-        while (scanner.hasNextLine()) strings.add(capFirstLetter(scanner.nextLine()));
-        System.out.println("After personAdjectives: " + strings.size());
-
-        try {scanner = new Scanner(file4);} catch(FileNotFoundException ignore) {}
-        while (scanner.hasNextLine()) strings.add(capFirstLetter(scanner.nextLine()));
-        System.out.println("After untrimmedMasterList :" + strings.size());
-
-        try (FileWriter writer = new FileWriter(file5, false)) {
-            for (String s : strings) {
-                writer.append(s).append("\n");
-            }
-        } catch(IOException e) {
-            System.out.println("didn't work");
-            e.printStackTrace();
-        }*/
-
-        // TODO: 3/28/23 we could add definitions to the words already? a lot more work, and not that much to gain,
-        //  but an option at least
-
         try {
-            Set<String> strings = new TreeSet<>();
-//            if (file.exists()) {
-//                Scanner scanner = new Scanner(file);
-//                while (scanner.hasNextLine()) {
-//                    StringBuilder s = new StringBuilder(scanner.nextLine().toLowerCase());
-//                    s.replace(0, 1, String.valueOf(Character.toUpperCase(s.charAt(0))));
-//                    strings.add(s.toString());
-//                }
-//            }
-            if (file2.exists()) {
-                Scanner scanner = new Scanner(file2);
-                while (scanner.hasNextLine()) strings.add(capFirstLetter(scanner.nextLine()));
-//                {
-//                    StringBuilder s = new StringBuilder(scanner.nextLine().toLowerCase());
-//                    s.replace(0, 1, String.valueOf(Character.toUpperCase(s.charAt(0))));
-//                    strings.add(s.toString());
-//                }
+            checkFilesExist();
+            
+            if (todayHasBeenDone()) {
+                printLastWord();
+                return;
             }
-            if (file3.exists()) {
-                Scanner scanner = new Scanner(file3);
-                while (scanner.hasNextLine()) strings.add(capFirstLetter(scanner.nextLine()));
-//                while (scanner.hasNextLine()) {
-//                    StringBuilder s = new StringBuilder(scanner.nextLine().toLowerCase());
-//                    s.replace(0, 1, String.valueOf(Character.toUpperCase(s.charAt(0))));
-//                    strings.add(s.toString());
-//                }
-            }
-//            for (String s : strings) {
-//                System.out.println(s);
-//            }
-            ArrayList<String> array = new ArrayList<>(strings);
-            while (strings.size() > 0) {
-                String s = array.get(new SecureRandom().nextInt(strings.size()));
+            
+            List<String> words = listWordsFromFiles(file2, file3);
+            while (! words.isEmpty()) { //"while" and not "if" in case you pick a repeat word
+                String s = words.get(new SecureRandom().nextInt(words.size()));
                 if (isNewWord(s)) {
-                    System.out.println(s);
+                    System.out.printf("Your word for the day is%n\t%s%n", s);
                     writeToHistory(s);
                     break;
                 }
@@ -97,11 +38,54 @@ public class Main {
             e.printStackTrace();
         }
     }
+    private static void checkFilesExist() {
+        if (!file.exists()) System.out.println("No file1");
+        if (!file2.exists()) System.out.println("No file2");
+        if (!file3.exists()) System.out.println("No file3");
+        if (!file4.exists()) System.out.println("No file4");
+        if (!file5.exists()) System.out.println("No file5");
+    }
+    private static boolean todayHasBeenDone() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("/Users/sterlite/Desktop/PetProjects/randomAdjectiveGenerator/src/listOfDaysDone.txt"));
+        String todayString = stringify(new Date());
+        while (scanner.hasNextLine()) {
+            if (scanner.nextLine().equals(todayString)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private static void printLastWord() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("/Users/sterlite/Desktop/PetProjects/randomAdjectiveGenerator/src/history.txt"));
+        String lastWord = "";
+        while (scanner.hasNextLine()) {
+            //note that history.txt has the words of the day on their own lines, so there is no split() needed, only strip()
+            lastWord = scanner.nextLine();
+        }
+        
+        System.out.println("You already got a word for today. That word was:\n" + lastWord);
+    }
     
-    public static boolean isNewWord(String s) throws FileNotFoundException {
+    private static List<String> listWordsFromFiles(File...files) throws FileNotFoundException {
+        Set<String> strings = new TreeSet<>();
+        for (File file : files) {
+            if (file.exists()) {
+                Scanner scanner = new Scanner(file2);
+                while (scanner.hasNextLine()) strings.add(capFirstLetter(scanner.nextLine()));
+            }
+        }
+        return new ArrayList<>(strings);
+    }
+    private static String capFirstLetter(String string) {
+        StringBuilder s = new StringBuilder(string.strip());
+        s.replace(0, 1, String.valueOf(Character.toUpperCase(s.charAt(0))));
+        return s.toString();
+    }
+    
+    private static boolean isNewWord(String s) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File("/Users/sterlite/Desktop/PetProjects/randomAdjectiveGenerator/src/history.txt"));
         while (scanner.hasNextLine()) {
-            //note that history.txt has the words of the day on their own lines, so there is no split() needed, only trim
+            //note that history.txt has the words of the day on their own lines, so there is no split() needed, only strip()
             if (scanner.nextLine().strip().equalsIgnoreCase(s)) {
                 return false;
             }
@@ -109,28 +93,40 @@ public class Main {
         return true;
     }
     
-    public static String capFirstLetter(String string) {
-        StringBuilder s = new StringBuilder(string.strip());
-        s.replace(0, 1, String.valueOf(Character.toUpperCase(s.charAt(0))));
-        return s.toString();
+    private static void writeToHistory(String adjective) {
+        try {
+            File history = new File("/Users/sterlite/Desktop/PetProjects/randomAdjectiveGenerator/src/history.txt");
+            FileWriter writer = new FileWriter(history, true);
+            
+            Date theDate = new Date();
+            String dateString = stringify(theDate);
+            
+            writer.append("\nThe adjective of the day for ");
+            writer.append(dateString);
+            writer.append(", generated at ");
+            writer.append(theDate.toString().substring(11, 16));  //the time of day
+            writer.append(", is:\n\t");
+            writer.append(adjective);
+            writer.flush();
+            writer.close();
+            
+            writeToListOfDaysDone(dateString);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-
-    public static String stringify(Date d) {
+    
+    private static String stringify(Date d) {
         StringBuilder bulk = new StringBuilder(d.toString());
-        StringBuilder toReturn = new StringBuilder();
-
-        toReturn.append(fullDay(bulk.substring(0,3)));   //day of week string
-        toReturn.append(", ");
-        toReturn.append(bulk.substring(8, 11)); //day number
-        toReturn.append(fullMonth(bulk.substring(4, 7)));  //month string
-        toReturn.append(" ");
-        toReturn.append(bulk.substring(24, 28));//year
-        toReturn.append(", generated at ");
-        toReturn.append(bulk.substring(11, 16));
-
-        return toReturn.toString();
+    
+        return fullDay(bulk.substring(0, 3)) +   //day of week string
+                ", " +
+                bulk.substring(8, 11) + //day number
+                fullMonth(bulk.substring(4, 7)) +  //month string
+                " " +
+                bulk.substring(24, 28);
     }
-    public static String fullDay(String abbrev) {
+    private static String fullDay(String abbrev) {
         return switch (abbrev) {
             case "Sun" -> "Sunday";
             case "Mon" -> "Monday";
@@ -142,7 +138,7 @@ public class Main {
             default -> "Error";
         };
     }
-    public static String fullMonth(String abbrev) {
+    private static String fullMonth(String abbrev) {
         return switch (abbrev) {
             case "Jan" -> "January";
             case "Feb" -> "February";
@@ -159,22 +155,15 @@ public class Main {
             default -> "Error";
         };
     }
-
-    public static void writeToHistory(String adjective) {
-        try {
-            File history = new File("/Users/sterlite/Desktop/PetProjects/randomAdjectiveGenerator/src/history.txt");
-            if (history.createNewFile()) {
-                System.out.println("File created: " + history.getName());
-            }
-
-            FileWriter writer = new FileWriter(history, true);
-            writer.append("\nThe adjective of the day for ");
-            writer.append(stringify(new Date())).append(", is:\n\t");
-            writer.append(adjective);
-            writer.flush();
-            writer.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    
+    private static void writeToListOfDaysDone(String dateString) throws IOException {
+        File listOfDaysDone = new File("/Users/sterlite/Desktop/PetProjects/randomAdjectiveGenerator/src/listOfDaysDone.txt");
+        
+        FileWriter writer = new FileWriter(listOfDaysDone, true);
+        writer.append("\n");
+        writer.append(dateString);
+        
+        writer.flush();
+        writer.close();
     }
 }
